5503a714c6508e60b487a22ebecf1911ddda1417
asteroid-J <speec35363@gmail.com>
2022-11-20 00:07:21 +0900

    수정함

--- front/signup.php
+++ front/signup.php
@@ -12,13 +12,16 @@
 </head>
 <body>
   
-  <form>
+  <form action="../backend/findid.php" method="POST">
     <div class="form">
       <h2 class="header">회원가입</h2>
-      <input type="text" name="makeid" placeholder="ID" class="id">
-      <input type="password" name="makepw" placeholder="Password" class="pw">
-      <input type="email" name="makeemail" placeholder="E-mail" class="email">
+      <input type="text" name="makename" placeholder="고객님의 성함을 입력해주세요" class="name">
+      <input type="text" name="makeid" placeholder="희망하시는 ID를 입력해주세요" class="id">
+      <input type="password" name="makepw" placeholder="비밀번호를 입력해주세요" class="pw">
+      <input type="email" name="makeemail" placeholder="이메일을 입력해주세요" class="email">
       <button class="maker" id="maker" onclick="pushAll();">제출</button>
+      <a href="findid.html">아이디찾기</a>
+      <a href="findpw.html">비밀번호찾기</a>
     </div>
   </form>
 
