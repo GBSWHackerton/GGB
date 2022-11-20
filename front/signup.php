87855211909406dbdcab610b282f6841a1d22bba
asteroid-J <speec35363@gmail.com>
2022-11-20 15:30:46 +0900

    수정 = crystal

--- front/signup.php
+++ front/signup.php
@@ -8,6 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap" rel="stylesheet">
+  <link rel="icon" href="../favicon/favicon.ico" type="favicon/x-icon">
   <title>Sign_up</title>
 </head>
 <body>
