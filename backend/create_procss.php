652d7e0f35fba51d938c939d31b8087425d8566a
cumedang <yusun2234sun@gmail.com>
2022-11-22 23:24:14 +0900

    샌즈

--- backend/create_procss.php
+++ backend/create_procss.php
@@ -1,3 +1,14 @@
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>가계부 생성</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
     session_start();
      //$conn=mysqli_connect("localhost","yusun2234sun1","sunlove8421!","yusun2234sun1");
@@ -21,5 +32,4 @@
     $sql1 = "INSERT INTO {$userid}(money,da) VALUES('{$money}','{$date}')";
     $result = mysqli_query($conn,$sql);
     $result1 = mysqli_query($conn,$sql1);
-
 ?>
