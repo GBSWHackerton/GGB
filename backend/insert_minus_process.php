6096de051c6aad52931886592125c50acde160a7
cumedang <yusun2234sun@gmail.com>
2022-11-23 23:52:04 +0900

    tlqkf

--- backend/insert_minus_process.php
+++ backend/insert_minus_process.php
@@ -5,10 +5,19 @@
     $how = $_POST['menu'];
     $price = (int)$_POST['price'];
     $tablename = $_SESSION['userid'];
-    $date = date("m-d");
-    $moneysql = "SELECT money FROM '{$tablename}' where no=1";
+    $date = date("y-m-d");
+    $moneysql = "SELECT money FROM {$tablename} WHERE no='1'";
     $result = mysqli_query($conn,$moneysql);
-    $resultprice = $result-$price;
-    $sql = "INSERT INTO '{$tablename}'(money,da,how) VALUES('{$resultprice}','{$date}','{$how}')";
+    $row = mysqli_fetch_array($result);
+    while($row){
+        $ez = array($row['money']);
+    }
+    echo $ez[0];
+    $intresult = (int)$ez[0];
+    $intprice = (int)$price;
+    echo $intresult;
+    $resultprice = $intresult-$intprice;
+    $sql = "INSERT INTO {$tablename}(money,da,because) VALUES('{$resultprice}','{$date}','{$how}')";
+    $result1 = mysqli_query($conn,$sql);
 
 ?>
